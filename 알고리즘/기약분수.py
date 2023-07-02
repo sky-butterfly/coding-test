4a7aef80992c807ae93f0544b14c271b973ab027
hk <sky_butterfly@naver.com>
2023-07-02 19:50:18 +0900

    add 알고리즘

---
+++ 알고리즘/기약분수.py
@@ -0,0 +1,4 @@
+from fractions import Fraction
+f = Fraction(2, 4)
+print(f.numerator)
+print(f.denominator)
---
+++ 프로그래머스/Level_2/네트워크.py
@@ -0,0 +1,17 @@
+def solution(n, computers):
+    answer = 0
+    check = []
+    
+    for i in range(n):
+        if i not in check:
+            check.append(i)
+            answer += 1
+            dfs(i, computers, check, n)
+    
+    return answer
+
+def dfs(i, computers, check, n):
+    for j in range(n):
+        if computers[i][j] == 1 and j not in check:
+            check.append(i)
+            dfs(j, computers, check, n)
